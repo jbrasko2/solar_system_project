309422cca4b25b4479a3b91363bc6e355343bb79
jbrasko <jbrasko0508@gmail.com>
2020-11-09 17:28:25 -0600

    minus check

--- lib/solar_system/cli.rb
+++ lib/solar_system/cli.rb
@@ -1,7 +1,7 @@
 class SolarSystem::CLI
 
     def call
-        puts "Welcome to the Solar System! check check check"
+        puts "Welcome to the Solar System! check"
         binding.pry
     end
 end
